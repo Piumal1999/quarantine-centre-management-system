0b4b021cba78dbdca35640c9e7f024d16f0af2ca
Piumal Rathnayake <piumalrathnayake@hotmail.com>
2022-02-25 21:52:38 +0530

    Add custom navbar (#2)

--- app/manage_client.php
+++ app/manage_client.php
@@ -1,5 +1,6 @@
 <?php
 include("session.php");
+$active_tab = "client";
 
 $rooms_query = "SELECT id FROM room ORDER BY id";
 $rooms = mysqli_query($db, $rooms_query);
@@ -105,71 +106,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <body class="g-sidenav-show bg-gray-100">
   <div class="min-height-300 bg-primary position-absolute w-100"></div>
-  <aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main">
-    <div class="sidenav-header">
-      <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-      <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/argon-dashboard/example_pages/dashboard.html " target="_blank">
-        <img src="../assets/img/logo-ct-dark.png" class="navbar-brand-img h-100" alt="main_logo">
-        <span class="ms-1 font-weight-bold">Quarantine Centre</span>
-      </a>
-    </div>
-    <hr class="horizontal dark mt-0">
-    <div class="w-auto " id="sidenav-collapse-main">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="../example_pages/dashboard.html">
-            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="ni ni-tv-2 text-primary text-sm opacity-10"></i>
-            </div>
-            <span class="nav-link-text ms-1">Dashboard</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" href="#">
-            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="fa fa-user-group text-warning text-sm opacity-10"></i>
-            </div>
-            <span class="nav-link-text ms-1">Clients</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link " href="#">
-            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="fa-solid fa-house-chimney-medical text-success text-sm"></i>
-            </div>
-            <span class="nav-link-text ms-1">Rooms</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link " href="#">
-            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="fa-solid fa-vial text-success text-sm"></i>
-            </div>
-            <span class="nav-link-text ms-1">Test Results</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link " href="#">
-            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="ni ni-credit-card text-success text-sm opacity-10"></i>
-            </div>
-            <span class="nav-link-text ms-1">Inventry</span>
-          </a>
-        </li>
-        <li class="nav-item mt-3">
-          <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Account pages</h6>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link " href="../example_pages/profile.html">
-            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="ni ni-single-02 text-dark text-sm opacity-10"></i>
-            </div>
-            <span class="nav-link-text ms-1">Profile</span>
-          </a>
-        </li>
-      </ul>
-    </div>
-  </aside>
+  <?php include("navbar.php") ?>
   <main class="main-content position-relative border-radius-lg ">
     <!-- Navbar -->
     <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur" data-scroll="false">
